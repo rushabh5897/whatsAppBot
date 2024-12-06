97e85dfe7108f76002c07097ce1dc89ad1917c55
Rushabh <rushabhpsancheti@gmail.com>
2024-12-06 21:30:31 +0530

    bug fix

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 from flask import Flask, request
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
-from data import data
 from get_claim import get_claim_details
 from get_warranty import get_warranty_details
 from session_history import view_session_history, save_session_to_json, get_last_message
@@ -11,10 +15,10 @@ from session_history import view_session_history, save_session_to_json, get_last
 app = Flask(__name__)
 
 # Twilio credentials (from your Twilio Console)
-TWILIO_ACCOUNT_SID = 'ACe6d90sdfgad86a7c7f4e5b1f60b10e4969c2'
-TWILIO_AUTH_TOKEN = 'f9f011494dasf5c1f6fdb6a98c00ef3f7125'
-TWILIO_WHATSAPP_NUMBER = 'whatsapp:+141dasfads55238886'  # Your Twilio Sandbox WhatsApp number
-TWILIO_PHONE_NUMBER = '+14155adsfadsf238886'
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_WHATSAPP_NUMBER = os.environ.get("TWILIO_WHATSAPP_NUMBER")
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 menu_options = {
@@ -34,18 +38,18 @@ def webhook():
     incoming_msg = request.values.get('Body', '').lower() or request.json.get('Body', '').lower()
     sender = request.values.get('From', '') or request.json.get('From', '').lower()
     sender = sender.removeprefix("whatsapp:")
-    print("sender", sender)
+
     # Create a response object
     response = MessagingResponse()
     print("last message :",get_last_message(sender))
 
     # Create the logic for your chatbot
-    if '1' or 'warranty details' in incoming_msg:
+    if '1'  in incoming_msg:
         response_list = get_warranty_details(sender)
         for res in response_list:
             response.message(res)
 
-    elif '2' or 'claim status' in incoming_msg:
+    elif '2' in incoming_msg:
         response_list = get_claim_details(sender)
         for res in response_list:
             response.message(res)
